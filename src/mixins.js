cf52243cd43d8c6b46418ba1f49b19e68c646bf5
Benjamin Crozat <benjamin@croz.at>
2017-07-15 10:27:39 +0200

    Clean up

--- src/mixins.js
+++ src/mixins.js
@@ -1,27 +1,56 @@
 export const modalSupport = {
     methods: {
+        forwardTitleSlot() {
+            if (!this.$refs.modal) {
+                return
+            }
+
+            if (this.$slots.title) {
+                this.$refs.modal.$slots.title = this.$slots.title
+            }
+        },
+
+        forwardCancelEvent() {
+            if (!this.$refs.modal) {
+                return
+            }
+
+            this.$refs.modal.$on('cancel', _ => {
+                this.$emit('cancel')
+            })
+        },
+
         cancel() {
+            if (!this.$refs.modal) {
+                return
+            }
+
             this.$refs.modal.visible = false
 
             this.$emit('cancel')
         },
 
         confirm() {
+            if (!this.$refs.modal) {
+                return
+            }
+
             this.$refs.modal.visible = false
 
             this.$emit('confirm')
         },
 
         setVisible(visible) {
+            if (!this.$refs.modal) {
+                return
+            }
+
             this.$refs.modal.visible = visible
         }
     },
 
     mounted() {
-        this.$refs.modal.$slots.title = this.$slots.title
-
-        this.$refs.modal.$on('cancel', _ => {
-            this.$emit('cancel')
-        })
+        this.forwardTitleSlot()
+        this.forwardCancelEvent()
     }
 }
