fe897e64394ff24dbad98cf85be002fc5c607b53
Benjamin Crozat <benjamin@croz.at>
2017-07-15 11:13:21 +0200

    Splitted tests and added more of them

--- test/unit/specs/Modal.spec.js
+++ test/unit/specs/Modal.spec.js
@@ -1,22 +1,62 @@
 import Vue from 'vue'
 import Modal from '@/Modal'
+import ModalButton from '@/ModalButton'
 
 describe('Modal.vue', () => {
+    const closeButtonText = 'Dismiss'
+    const titleText = 'Hello, World!'
+    const content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur doloribus, id voluptatum alias dignissimos, dolore rem cum autem velit mollitia consectetur quas fugit ad, error facilis ratione veritatis. Voluptate, laborum!'
+
+    const vm = new Vue({
+        components: {
+            Modal,
+            ModalButton
+        },
+
+        template: `<modal close-button-text="${closeButtonText}" ref="modal">
+                       <h1 slot="title">${titleText}</h1>
+
+                       <p>${content}</p>
+
+                       <modal-button slot="buttons"></modal-button>
+                       <modal-button :autofocus="true" :validate="true" slot="buttons"></modal-button>
+                       <modal-button :destructive="true" slot="buttons"></modal-button>
+                   </modal>`
+    }).$mount()
+
+    vm.$refs.modal.visible = true
+
     it('should render custom close button text', done => {
-        const Constructor = Vue.extend(Modal)
-
-        const vm = new Constructor({
-            propsData: {
-                closeButtonText: 'Fermer'
-            }
-        }).$mount()
-
-        vm.visible = true
-
         vm.$nextTick(() => {
             expect(vm.$el.querySelector('.vmc__close-btn').textContent.trim())
-                .to
-                .equal('Fermer')
+                .to.equal(closeButtonText)
+
+            done()
+        })
+    })
+
+    it('should render the title', done => {
+        vm.$nextTick(() => {
+            expect(vm.$el.querySelector('.vmc__header > h1').textContent.trim())
+                .to.equal(titleText)
+
+            done()
+        })
+    })
+
+    it('should render the content', done => {
+        vm.$nextTick(() => {
+            expect(vm.$el.querySelector('.vmc__content').textContent.trim())
+                .to.equal(content)
+
+            done()
+        })
+    })
+
+    it('should render three buttons', done => {
+        vm.$nextTick(() => {
+            expect(vm.$el.querySelectorAll('.vmc__btn').length)
+                .to.equal(3)
 
             done()
         })
---
+++ test/unit/specs/ModalButton.spec.js
@@ -0,0 +1,72 @@
+import Vue from 'vue'
+import ModalButton from '@/ModalButton'
+
+describe('ModalButton.vue', () => {
+    it('should render as validation button', done => {
+        const Constructor = Vue.extend(ModalButton)
+
+        const vm = new Constructor({
+            propsData: {
+                validate: true
+            }
+        }).$mount()
+
+        vm.$nextTick(() => {
+            expect(vm.$el.classList.contains('vmc__btn--validate'))
+                .to.be.true
+
+            done()
+        })
+    })
+
+    it('should render autofocused', done => {
+        const Constructor = Vue.extend(ModalButton)
+
+        const vm = new Constructor({
+            propsData: {
+                autofocus: true
+            }
+        }).$mount()
+
+        vm.$nextTick(() => {
+            expect(vm.$el.getAttribute('autofocus'))
+                .to.not.be.null
+
+            done()
+        })
+    })
+
+    it('should render as a delete button', done => {
+        const Constructor = Vue.extend(ModalButton)
+
+        const vm = new Constructor({
+            propsData: {
+                destructive: true
+            }
+        }).$mount()
+
+        vm.$nextTick(() => {
+            expect(vm.$el.classList.contains('vmc__btn--destructive'))
+                .to.be.true
+
+            done()
+        })
+    })
+
+    it('should render as a submit type', done => {
+        const Constructor = Vue.extend(ModalButton)
+
+        const vm = new Constructor({
+            propsData: {
+                type: 'submit'
+            }
+        }).$mount()
+
+        vm.$nextTick(() => {
+            expect(vm.$el.getAttribute('type'))
+                .to.equal('submit')
+
+            done()
+        })
+    })
+})
